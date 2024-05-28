0447563647ac609c45a727310f25bca6fc9691e3
haihoangvua0 <167439221+haihoangvua0@users.noreply.github.com>
2024-05-28 16:00:52 +0700

    Create Hien_thi_ngay_trong_tuan.py

---
+++ Hien_thi_ngay_trong_tuan.py
@@ -0,0 +1,19 @@
+a = input("Enter a number to predict date: ")
+date = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+if a == 2:
+  print(f"The date is {date[1]}")
+elif a == 3:
+  print(f"The date is {date[2]}")
+elif a == 4:
+  print(f"The date is {date[3]}")
+elif a == 5:
+  print(f"The date is {date[4]}")
+elif a == 6:
+  print(f"The date is {date[5]}")
+elif a == 7:
+  print(f"The date is {date[6]}")
+elif a == 8:
+  print(f"The date is {date[7]}")
+else:
+  print("Error!!!")
